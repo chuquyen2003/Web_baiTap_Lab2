2b7dc0b1e99d9c42a456a5bdf329602e0654c285
chuquyen2003 <chuquyen464@gmail.com>
2023-09-21 00:13:12 +0700

    n

---
+++ Models/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Bai_Lap1.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; } //Item Id
+        public string Name { get; set; } //Item name
+        public string Link { get; set; } //Item label
+
+    }
+}
